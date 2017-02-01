0a4c3dae3d4eb62e609e11bb683bc699a0e69a44
Joey Gibson <joey@joeygibson.com>
2017-01-31 23:07:34 -0500

    Converted to use Cobra

--- issues.go
+++ issues.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
-	"net/url"
-	"strings"
 	"github.com/google/go-github/github"
 	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+	"net/url"
+	"os"
 	"strconv"
+	"strings"
 )
 
 var (
-	numberOfIssues = flag.Int("issues", -1, "maximum number of issues to retrieve")
-	repo = flag.String("repo", "", "repository to fetch issues from")
+	numberOfIssues int
+	repo           string
+	rootCmd        = &cobra.Command{
+		Use:   "issues",
+		Short: "Shows issues from a Github repo",
+		Long:  "Currently only works with public repos",
+		Run:   CmdRoot,
+	}
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	flag.Parse()
-
-	if repo == nil {
-		fmt.Errorf("No repo specified\n")
-		flag.Usage()
+func CmdRoot(cmd *cobra.Command, _ []string) {
+	if repo == "" {
+		fmt.Printf("%v\n", "No repo specified")
+		cmd.Help()
 		os.Exit(1)
 	}
 
-	u, err := url.Parse(*repo)
+	u, err := url.Parse(repo)
 
 	if err != nil {
-		fmt.Errorf("Invalid repo URL", err)
+		fmt.Printf("Invalid repo URL: %v\n", err)
 		os.Exit(2)
 	}
 
 	path := strings.Split(u.EscapedPath(), "/")
+
 	if len(path) < 3 {
-		fmt.Errorf("Invalid Github URL")
+		fmt.Printf("Invalid Github URL: %v\n", u)
 		os.Exit(2)
 	}
 
@@ -52,10 +52,10 @@ func main() {
 	var issues []*github.Issue
 
 	// if numberOfIssues < 0, we fetch all issues
-	for len(issues) <= *numberOfIssues || *numberOfIssues < 0 {
+	for len(issues) <= numberOfIssues || numberOfIssues < 0 {
 		newIssues, resp, err := client.Issues.ListByRepo(path[1], path[2], opt)
 		if err != nil {
-			fmt.Errorf("error fetching issues for %v/%v", path[1], path[2], err)
+			fmt.Printf("error fetching issues for %v/%v: %v\n", path[1], path[2], err)
 			os.Exit(3)
 		}
 
@@ -78,7 +78,7 @@ func main() {
 	table.SetHeader([]string{"index", "number", "created_at", "title"})
 
 	for index, issue := range issues {
-		if index == *numberOfIssues {
+		if index == numberOfIssues {
 			break
 		}
 
@@ -93,4 +93,12 @@ func main() {
 
 	table.SetRowLine(true)
 	table.Render()
+}
+
+func main() {
+	rootCmd.Flags().IntVarP(&numberOfIssues, "number-of-issues",
+		"n", -1, "the number of issues to retrieve")
+	rootCmd.Flags().StringVarP(&repo, "repo", "r", "", "the Github URL to retrieve")
+
+	rootCmd.Execute()
 }
